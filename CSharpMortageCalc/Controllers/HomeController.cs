83277c3518b294532dce4c811a32394da3e46d94
JohnGriffith <johnathon.griffith@gmail.com>
2021-07-24 16:35:48 -0700

    Add project files.

---
+++ CSharpMortageCalc/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using CSharpMortageCalc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSharpMortageCalc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult App()
+        {
+            Loan loan = new();
+            loan.Payment = 0.0m;
+            loan.Amount = 300000m;
+            loan.TotalInterest = 0.0m;
+            loan.TotalCost = 0.0m;
+            loan.Rate = 5.0m;
+            loan.Term = 360;
+            return View(loan);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
